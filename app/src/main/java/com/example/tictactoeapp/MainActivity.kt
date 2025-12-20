58409c6df35163ce83254eff1ab7e37d8816d741
annaeden1 <annaeden1@gmail.com>
2025-12-20 14:43:54 +0200

    add check for win and add disabling button

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : ComponentActivity() {
     private var current_player: Char = 'X'
     private var play_again_button: Button? = null
 
+    private var board = Array(3) { CharArray(3 ) { ' ' } }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,7 +25,6 @@ class MainActivity : ComponentActivity() {
         playersTurn = findViewById<TextView>(R.id.players_turn_text)
         winner = findViewById<TextView>(R.id.winner_text)
         play_again_button = findViewById<Button>(R.id.play_again_button)
-
 
         play_again_button?.isEnabled = false
         playersTurn?.text = "It's ${current_player}'s turn"
@@ -40,17 +41,19 @@ class MainActivity : ComponentActivity() {
                 )
                 val currentButton: Button = findViewById<Button>(id)
                 currentButton.setOnClickListener {
-                    playATurn(currentButton)
+                    playATurn(currentButton, i, j)
                 }
             }
         }
     }
 
-    fun playATurn(currentButton: Button) {
+    fun playATurn(currentButton: Button, row: Int, col: Int) {
         currentButton.text = "${current_player}"
+        board[row][col] = current_player
 
-        if (checkForAWin(currentButton)) {
+        if (checkForAWin(row, col)) {
           winner?.text = "${current_player} wins!"
+          disableBoard()
           play_again_button?.isEnabled = true
           play_again_button?.setOnClickListener {
             startANewGame()
@@ -58,6 +61,7 @@ class MainActivity : ComponentActivity() {
         } else {
             if (checkForADraw()) {
                 winner?.text = "It's a draw!"
+                disableBoard()
                 play_again_button?.isEnabled = true
                 play_again_button?.setOnClickListener {
                     startANewGame()
@@ -73,10 +77,13 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun checkForAWin(currentButton: Button): Boolean {
-        // go over it's row
-        //go over it's col
-        //go over it's / \
+    fun checkForAWin(row: Int, col: Int): Boolean {
+        if ((0..2).all { board[row][it] == current_player }) return true
+        if ((0..2).all { board[it][col] == current_player }) return true
+
+        if (row == col && (0..2).all { board[it][it] == current_player }) return true
+        if (row + col == 2 && (0..2).all { board[it][2 - it] == current_player }) return true
+
         return false
     }
 
@@ -98,6 +105,20 @@ class MainActivity : ComponentActivity() {
         return true
     }
 
+    fun disableBoard() {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                val id = resources.getIdentifier(
+                    "button_${i}_${j}",
+                    "id",
+                    packageName
+                )
+                val currentButton: Button = findViewById<Button>(id)
+                currentButton.isEnabled = false
+            }
+        }
+    }
+
     fun cleanBoard() {
         for (i in 0..2) {
             for (j in 0..2) {
@@ -107,7 +128,9 @@ class MainActivity : ComponentActivity() {
                     packageName
                 )
                 val currentButton: Button = findViewById<Button>(id)
+                currentButton.isEnabled = true
                 currentButton.text = ""
+                board[i][j] = ' '
             }
         }
     }
